8c74f09a2306de874547a3e674d3ea8237fb38bf
diamant1991 <diamant1988@mail.ru>
2015-05-26 18:05:50 +0300

    first files

---
+++ js/filter.js
@@ -0,0 +1,44 @@
+jQuery("#area").slider({
+	min: 1,
+	max: 80,
+	values: [1,80],
+	range: true,
+	stop: function(event, ui) {
+		jQuery("#min-area").val(jQuery("#area").slider("values",0));
+		jQuery("#max-area").val(jQuery("#area").slider("values",1));
+    },
+    slide: function(event, ui){
+		jQuery("#min-area").val(jQuery("#area").slider("values",0));
+		jQuery("#max-area").val(jQuery("#area").slider("values",1));
+    }
+});
+
+	jQuery("#floor").slider({
+	min: 1,
+	max: 20,
+	values: [1,20],
+	range: true,
+	stop: function(event, ui) {
+		jQuery("#min-floor").val(jQuery("#floor").slider("values",0));
+		jQuery("#max-floor").val(jQuery("#floor").slider("values",1));
+    },
+    slide: function(event, ui){
+		jQuery("#min-floor").val(jQuery("#floor").slider("values",0));
+		jQuery("#max-floor").val(jQuery("#floor").slider("values",1));
+    }
+});
+
+	jQuery("#price").slider({
+	min: 100000,
+	max: 10000000,
+	values: [100000,10000000],
+	range: true,
+	stop: function(event, ui) {
+		jQuery("#min-price").val(jQuery("#price").slider("values",0));
+		jQuery("#max-price").val(jQuery("#price").slider("values",1));
+    },
+    slide: function(event, ui){
+		jQuery("#min-price").val(jQuery("#price").slider("values",0));
+		jQuery("#max-price").val(jQuery("#price").slider("values",1));
+    }
+});
